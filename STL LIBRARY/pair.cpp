0fc5c096c96d59a7a747211dd41bea1b4b43934c
Manver Singh <manveersingh0501l@gmail.com>
2025-12-25 20:53:18 +0530

    added pairs

---
+++ STL LIBRARY/pair.cpp
@@ -0,0 +1,11 @@
+// A pair is a container that stores two values together.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // pair intialization
+    pair<int , string>p;
+    p.first = 1;
+    p.second = "abc";
+    cout<<p.first<<" "<<p.second<<endl;
+    
+}
