9b925fd351e02d87d6f5622346f80f202c91d2c9
Manver Singh <manveersingh0501l@gmail.com>
2025-12-26 16:33:54 +0530

    added unordered set

--- STL LIBRARY/setandunorderedset.cpp
+++ STL LIBRARY/setandunorderedset.cpp
@@ -31,6 +31,29 @@ int main(){
     cout<<"\nSize of set: "<<s.size()<<endl;  // 2
 
 
+    //unordered set
+    //1. intializing unordered set
+    unordered_set<int> us;
+    us.insert(5);
+    us.insert(2);
+    us.insert(8);
+    us.insert(2);   // ignored (duplicate)
+    //2. traversing unordered set+
+    cout<<"Unordered set elements: ";
+    for(auto x: us){
+        cout<<x<<" ";   // order may vary
+    }
+    cout<<endl;
+
+    //uses of unordered set
+    // remove duplicates from vector
+    vector<int> vec = {1, 2, 2, 3, 4, 4, 5};
+    unordered_set<int> uset(vec.begin(), vec.end());
+    for(auto x: uset){
+        cout<<x<<" ";   // 1 2 3 4 5 (order may vary)
+    }
+    return 0;
+
 
 
 
