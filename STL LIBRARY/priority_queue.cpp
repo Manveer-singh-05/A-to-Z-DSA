4abd9bbd192e661fd9079122351cefd1e3de0fe1
Manver Singh <manveersingh0501l@gmail.com>
2026-01-01 18:46:08 +0530

    added min heap logic

--- STL LIBRARY/priority_queue.cpp
+++ STL LIBRARY/priority_queue.cpp
@@ -9,5 +9,15 @@ int main(){
     cout << "Top element: " << pq.top() << endl; // Should print 20
     pq.pop();
     cout << "Top element after pop: " << pq.top() << endl; // Should print 15
+
+    // Min-heap using greater<int>
+    priority_queue<int, vector<int>, greater<int>> min_pq;
+    min_pq.push(10);
+    min_pq.push(20);
+    min_pq.push(15);
+    cout << "Top element of min-heap: " << min_pq.top() << endl; // Should print 10
+    min_pq.pop();
+    cout << "Top element of min-heap after pop: " << min_pq.top() << endl; // Should print 15
+    
     return 0;
 }
