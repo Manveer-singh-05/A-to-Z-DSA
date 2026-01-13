6817635a58894067cebcd2cc41be964473d25201
Manver Singh <manveersingh0501l@gmail.com>
2026-01-13 18:54:37 +0530

    added deletion

---
+++ Array/deletionatbeg.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Array before deletion: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // Deletion at beginning
+    for(int i=0;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    n--; // Reduce the size of the array after deletion
+    cout<<"Array after deletion: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
