dc2820c964d355077d411db4abefc111d97703a5
Manver Singh <manveersingh0501l@gmail.com>
2026-02-14 23:37:08 +0530

    added armstrong number

--- basic math logic/armstrongnumber.cpp
+++ basic math logic/armstrongnumber.cpp
@@ -1,7 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
+int power(int base, int exp){
+    int result = 1;
+    while(exp--){
+        result *= base;
+    }
+    return result;
+}
+
 int main(){
     int n;
     cin>>n;
-    
+    int length = to_string(n).length();
+    int sum = 0 ;
+    int temp = n;
+    while(temp>0){
+        int rem  = temp%10;
+        sum+=power(rem, length);
+        temp /=10;
+    }
+    if(sum == n) cout<<"Armstrong Number";
+    else cout<<"Not an Armstrong Number";
 }
