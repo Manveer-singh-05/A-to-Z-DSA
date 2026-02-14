c47733247802711e63eb1c2a25009c9ed75f2e46
Manver Singh <manveersingh0501l@gmail.com>
2026-02-14 23:49:38 +0530

    added optimum approach

--- basic math logic/printalldivisors.cpp
+++ basic math logic/printalldivisors.cpp
@@ -2,14 +2,31 @@
 using namespace std;
 int main(){
     // brute force
+    // int n;
+    // cin>>n;
+    // vector<int> divisors;
+    // for(int i=1;i<=n;i++){
+    //     if(n%i==0){
+    //         divisors.push_back(i);
+    //     }
+    // }
+    // for(auto it: divisors){
+    //     cout<<it<<" ";
+    // }
+
+    //optimal approach
     int n;
     cin>>n;
     vector<int> divisors;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i*i<=n;i++){
         if(n%i==0){
             divisors.push_back(i);
+            if(i!=n/i){
+                divisors.push_back(n/i);
+            }
         }
     }
+    sort(divisors.begin(), divisors.end());
     for(auto it: divisors){
         cout<<it<<" ";
     }
