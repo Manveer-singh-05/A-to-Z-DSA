4d22efddbbcc44ddd3677c76aeecf70dcebe06da
Manver Singh <manveersingh0501l@gmail.com>
2026-02-15 11:46:30 +0530

    added optimal approach

--- basic math logic/checkifprimeornot.cpp
+++ basic math logic/checkifprimeornot.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool isPrime(int n){
+    if(n <= 1) return false;
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
+
+    for(int i = 3; i*i <= n; i += 2){
+        if(n % i == 0)
+            return false;
+    }
+    return true;
+}
+
 int main(){
+    // optimal approach 
+    // int n;
+    // cin>>n;
+    // bool isPrime=true;
+    // if(n<=1){
+    //     cout<<"Not Prime";
+    //     return 0;
+    // }
+    // for(int i=2;i*i<=n;i++){
+    //     if(n%i==0){
+    //         isPrime=false;
+    //         cout<<"Not Prime";
+    //         return 0;
+    //     }
+    // }
+    // if(isPrime){
+    //     cout<<"Prime";
+    // }
+
+    //most optimal approach 
     int n;
     cin>>n;
-    bool isPrime=true;
-    if(n<=1){
-        cout<<"Not Prime";
-        return 0;
-    }
-    for(int i=2;i*i<=n;i++){
-        if(n%i==0){
-            isPrime=false;
-            cout<<"Not Prime";
-            return 0;
-        }
-    }
-    if(isPrime){
-        cout<<"Prime";
-    }
+    isPrime(n) ? cout<<"Prime" : cout<<"Not Prime";
 
 }
