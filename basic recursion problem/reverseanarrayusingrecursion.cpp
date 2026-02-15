bd1a3ed26c65068eadd7dc88e2da50cb3b7885a3
Manver Singh <manveersingh0501l@gmail.com>
2026-02-15 13:02:12 +0530

    added reverse an array

---
+++ basic recursion problem/reverseanarrayusingrecursion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void rever(int arr[], int i, int n){
+    if(i>=n/2) return;
+    swap(arr[i], arr[n-i-1]);
+    rever(arr, i+1, n);
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    rever(arr, 0, n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
