348f6e2ea4eb7776cd73ee8a3660f8fb8879f4a4
Manver Singh <manveersingh0501l@gmail.com>
2026-02-15 14:06:52 +0530

    Added optimum appraoch

--- basic recursion problem/fibonacciseriesusingrecursion.cpp
+++ basic recursion problem/fibonacciseriesusingrecursion.cpp
@@ -1,26 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-int fibonacci(int N) {
+// int fibonacci(int N) {
    
-    if (N <= 1) {
-        return N;
-    }
-    int last = fibonacci(N - 1);  
-    int slast = fibonacci(N - 2); 
+//     if (N <= 1) {
+//         return N;
+//     }
+//     int last = fibonacci(N - 1);  
+//     int slast = fibonacci(N - 2); 
 
-    return last + slast;
+//     return last + slast;
+// }
+
+// more optimum approach
+void printFibo(int n, int a, int b){
+    if(n == 0)
+        return;
+
+    cout << a << " ";
+
+    printFibo(n - 1, b, a + b);
 }
 
+
 int main(){
+    // int n;
+    // cin>>n;
+    // int a= 0; 
+    // int b =1;
+    // cout<<"The Fibonacci Series up to "<<n<<"th term:"<<endl;
+    // cout<<a<<" "<<b<<" ";
+    // for(int i=2; i<=n; i++){
+    //    cout<<fibonacci(i)<<" ";
+    // }
+
+    //optimised approach
+
     int n;
-    cin>>n;
-    int a= 0; 
-    int b =1;
-    cout<<"The Fibonacci Series up to "<<n<<"th term:"<<endl;
-    cout<<a<<" "<<b<<" ";
-    for(int i=2; i<=n; i++){
-       cout<<fibonacci(i)<<" ";
-    }
+    cin >> n;
 
+    cout << "Fibonacci Series:\n";
+    printFibo(n, 0, 1);
 
+    return 0;
 }
