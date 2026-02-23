b9d3ade987dfa0bea4f50a102ed976ca316f25cf
Manver Singh <manveersingh0501l@gmail.com>
2026-02-23 10:48:31 +0530

    Added a dutch flag algo

---
+++ named algorithm/dutchflagproblem.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int low =0;
+    int mid =0;
+    int high = n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[mid++], arr[low++]);
+        }
+        else if (arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[high--]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
