2bc0862a4a4f30e321461a286105e2ed089934c4
Manver Singh <manveersingh0501l@gmail.com>
2026-03-10 22:52:23 +0530

    added p16cpp

---
+++ patterns/p16.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        char a = 'A'+i-1;
+        for(int j=1;j<=i;j++){
+            cout<<a<<" ";
+        }
+        cout<<endl;
+    }
+}
