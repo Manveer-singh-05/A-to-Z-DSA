85682110e7ecd43b975d4efff223e3e5df5aef8d
Manver Singh <manveersingh0501l@gmail.com>
2026-07-08 22:14:55 +0530

    Added some fixes

--- dp/longestcommonsubsequence/longestcommonsubstringtopdown.cpp
+++ dp/longestcommonsubsequence/longestcommonsubstringtopdown.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int lcs(string &s, string&t, int n, int m, vector<vector<int>>&k){
+int lcs(string &s, string&t, int n, int m, vector<vector<int>>&k, int count){
     if(n==0 || m==0){
-        return 0;
+        return count;
     }
     if(k[n][m]!=-1){
         return k[n][m];
     }
     if(s[n-1]==t[m-1]){
-       return k[n][m] = 1+lcs(s,t,n-1,m-1,k);
+        k[n][m] = 1+lcs(s,t,n-1,m-1,k);
     }
     else{
-      return k[n][m] = max(lcs(s,t,n-1,m,k), lcs(s,t,n,m-1,k));
+        k[n][m] = max(lcs(s,t,n-1,m,k,0), lcs(s,t,n,m-1,k,0));
     }
+
 }
 int main(){
     string s,t;
@@ -20,5 +21,5 @@ int main(){
     int n= s.size();
     int m = t.size();
     vector<vector<int>>k(n+1, vector<int>(m+1, -1));
-    cout<<lcs(s,t,n,m,k);
+    cout<<lcs(s,t,n,m,k,0);
 }
