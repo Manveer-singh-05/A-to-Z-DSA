d7d270d406de14e9ff29956b952b40c6ef4bc2fb
Manver Singh <manveersingh0501l@gmail.com>
2026-07-08 22:18:35 +0530

    added longest common substring using top down approach

--- dp/longestcommonsubsequence/longestcommonsubstringtopdown.cpp
+++ dp/longestcommonsubsequence/longestcommonsubstringtopdown.cpp
@@ -1,25 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int lcs(string &s, string&t, int n, int m, vector<vector<int>>&k, int count){
-    if(n==0 || m==0){
+
+int solve(string &s1, string &s2, int n, int m, int count,
+          vector<vector<vector<int>>> &dp) {
+
+    if (n == 0 || m == 0)
         return count;
-    }
-    if(k[n][m]!=-1){
-        return k[n][m];
-    }
-    if(s[n-1]==t[m-1]){
-        k[n][m] = 1+lcs(s,t,n-1,m-1,k);
-    }
-    else{
-        k[n][m] = max(lcs(s,t,n-1,m,k,0), lcs(s,t,n,m-1,k,0));
+
+    if (dp[n][m][count] != -1)
+        return dp[n][m][count];
+
+    int ans = count;
+
+    if (s1[n - 1] == s2[m - 1]) {
+        ans = solve(s1, s2, n - 1, m - 1, count + 1, dp);
     }
 
+    int op1 = solve(s1, s2, n - 1, m, 0, dp);
+    int op2 = solve(s1, s2, n, m - 1, 0, dp);
+
+    return dp[n][m][count] = max(ans, max(op1, op2));
 }
-int main(){
-    string s,t;
-    cin>>s>>t;
-    int n= s.size();
-    int m = t.size();
-    vector<vector<int>>k(n+1, vector<int>(m+1, -1));
-    cout<<lcs(s,t,n,m,k,0);
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int n = s1.size();
+    int m = s2.size();
+
+    vector<vector<vector<int>>> dp(
+        n + 1,
+        vector<vector<int>>(m + 1,
+            vector<int>(min(n, m) + 1, -1)));
+
+    cout << solve(s1, s2, n, m, 0, dp);
+
+    return 0;
 }
