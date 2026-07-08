c86eecb869f64a57f58dda30f1df8e7f3c1e4afa
Manver Singh <manveersingh0501l@gmail.com>
2026-07-08 22:09:24 +0530

    Added longest common substring using bottom up approach

---
+++ dp/longestcommonsubsequence/longestcommonsubstringbottomup.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    int n = s1.size();
+    int m = s2.size();
+    int count  =0;
+    vector<vector<int>>k(n+1, vector<int>(m+1, 0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                k[i][j] = 1 + k[i-1][j-1];
+                count = max(count,k[i][j]);
+            }
+            else{
+                k[i][j] = 0;
+            }
+        }
+    }
+    cout<<count;
+}
