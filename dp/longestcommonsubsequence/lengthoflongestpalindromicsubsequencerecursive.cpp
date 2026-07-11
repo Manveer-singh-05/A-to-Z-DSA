ff49748ee254a9a43270eefc037bcd172cb10e26
Manver Singh <manveersingh0501l@gmail.com>
2026-07-11 16:32:17 +0530

    Added lenght of longest palindroic subsequence using recursive approach

---
+++ dp/longestcommonsubsequence/lengthoflongestpalindromicsubsequencerecursive.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int lcs(string &s1, string &s2, int n, int m){
+    if(n==0 || m==0){
+        return 0;
+    }
+    else if(s1[n-1]==s2[m-1]){
+        return 1 + lcs(s1,s2,n-1,m-1);
+    }
+    else{
+        return  max(lcs(s1,s2,n,m-1), lcs(s1,s2,n-1,m));
+    }
+}
+int main(){
+    string s1;
+    cin>>s1;
+    string s2;
+    s2 = s1;
+    reverse(s2.begin(), s2.end());
+    int n = s1.size();
+    int m = n;
+    vector<vector<int>>k(n+1, vector<int>(m+1, -1));
+    cout<<lcs(s1,s2,n,m);
+}
