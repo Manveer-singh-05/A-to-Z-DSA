bd378e3747b1777b44713f590933a85a94a4b796
Manver Singh <manveersingh0501l@gmail.com>
2026-07-13 21:58:13 +0530

    Added minimum number of insertion in string to make it palindrome using bottom up

---
+++ dp/longestcommonsubsequence/minimumnoofinsertiontopalindromebottomup.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1;
+    cin>>s1;
+    string s2;
+    s2=  s1;
+    reverse(s2.begin(), s2.end());
+    int n  =s1.size();
+    int m = n;
+    vector<vector<int>>k(n+1, vector<int>(m+1, 0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1] == s2[j-1]){
+                k[i][j] = 1+ k[i-1][j-1];
+            }
+            else{
+                k[i][j]  = max(k[i-1][j], k[i][j-1]);
+            }
+        }
+    }
+    cout<<n-k[n][m];
+    return 0;
+}
