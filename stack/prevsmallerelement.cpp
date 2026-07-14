4b2c0258d3858f2a1e429d3a1ee83480206558f2
Manver Singh <manveersingh0501l@gmail.com>
2026-07-14 18:36:43 +0530

    Added prev smaller element using stack

---
+++ stack/prevsmallerelement.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>ans(n);
+    stack<int>st;
+    for(int i=0;i<n;i++){
+        while(!st.empty() && arr[i]<=st.top()){
+            st.pop();
+        }
+        if(st.empty()){
+            ans[i]  =-1;
+        }
+        else{
+            ans[i]  = st.top();
+        }
+        st.push(arr[i]);
+    }
+    for(int x: ans){
+        cout<<x<<" ";
+    }
+}
