3e50a321be30e7e2ef77a3cc474f16b00e8edb7b
Manver Singh <manveersingh0501l@gmail.com>
2026-08-23 15:38:21 +0530

    added graph implentation using adjaceny list

---
+++ graph/ajacenylistgraph.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class graph{
+    public:
+    unordered_map<int,list<int>>adj;
+    void addedge(int u,int v,bool direction){
+        //direction=0->undirected
+        //direction=1->directed
+        adj[u].push_back(v);
+        if(direction==0){
+            adj[v].push_back(u);
+        }
+
+    }
+    void printadjlist(){
+        for(auto i: adj){
+            cout<<i.first<<"->";
+            for(auto it : i.second){
+                cout<<it<<" ";
+            }
+            cout<<endl;
+        }
+    }
+};
+int main(){
+    int n;
+    cout<<"Enter the number of nodes";
+    cin>>n;
+    int m;
+    cout<<"Enter the number of edges";
+    cin>>m;
+    graph g;
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        g.addedge(u,v,0);
+    }
+    g.printadjlist();
+}
